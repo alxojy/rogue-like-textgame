6b8ca0313598cd8fef328d18e7c8d8a566409acb
megan o <mooi0004@student.monash.edu>
2019-05-02 23:17:53 +0800

    Fix distance methods in StunBehaviour & FollowBehaviour

--- src/game/RocketPad.java
+++ src/game/RocketPad.java
@@ -12,33 +12,21 @@ public class RocketPad extends Ground {
     @Override
     public Actions allowableActions(Actor actor, Location location, String direction) {
         Actions actions = new Actions();
+        ActorLocations actorLocations = new ActorLocations();
 
-        /*
-        boolean firstCond = false, secondCond = false;
-        for (Item currentItem: actor.getInventory()){
-            if (currentItem.hasSkill(GameSkills.BUILDROCKETBASE)) {
-                firstCond = true;
-            }
-            else if (currentItem.hasSkill(GameSkills.BUILDROCKETTOP)) {
-                secondCond = true;
-            }
-        }
-        if (firstCond && secondCond) {
-            if (actor instanceof Player) {
-                actions.add(new BuildRocketAction(actor));
-            }
-        }*/
         if (actor instanceof GamePlayer) {
             if (checkItems(location)) {
                 actions.add(new BuildRocketAction(actor));
-                ((GamePlayer) actor).removePlayerFromMap((GamePlayer)actor);
-
+                for (Actor actorInLocation : actorLocations) {
+                    ((Enemy) actorInLocation).removeActor(actorInLocation);
+                }
+                ((GamePlayer) actor).removePlayerFromMap((GamePlayer) actor);
             }
         }
         return actions;
     }
 
-    public boolean checkItems(Location location){
+    private boolean checkItems(Location location){
         boolean retVal = false;
         Item rocketBody = null, rocketEngine= null ;
         List<Item> itemList = location.getItems();
