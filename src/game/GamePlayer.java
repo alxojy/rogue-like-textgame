edf082b8ae21ec2d9f7793c17357ac922ccd125b
yhew0003 <yhew0003@student.monash.edu>
2019-05-02 23:22:29 +0800

    Fixed Rocket Body ( Logic Error ) - printed drop action twice

--- src/game/GamePlayer.java
+++ src/game/GamePlayer.java
@@ -2,16 +2,47 @@ package game;
 
 import edu.monash.fit2099.engine.*;
 
+/**
+ *
+ * The GamePlayer extends from Player.
+ * GamePlayer has additional MaxCounter attribute and stunnerPlayer boolean variable.
+ * These additional instance variables enables GamePlayer to be stunned by Ninja.
+ *
+ */
 public class GamePlayer extends Player {
 
     private MaxCounter counter = new MaxCounter(3);
     private boolean stunnedPlayer = false;
     private GameMap map ;
 
+    /**
+     * Constructor.
+     *
+     * @param name Name to call the GamePlayer in the UI
+     * @param displayChar Character to display the GamePlayer in UI
+     * @param priority How early in the turn the GamePlayer can act
+     * @param hitPoints GamePlayer's starting number of hitpoints
+     */
     public GamePlayer(String name, char displayChar, int priority, int hitPoints) {
         super(name, displayChar, priority, hitPoints);
     }
 
+    /**
+     * Overrides the method from Player
+     *
+     * Additional feature
+     * This overridden method
+     * 1. increments and resets the counter attribute.
+     * 2. changes the status of stunnedPlayer
+     *
+     * This allows the player to be stunned for TWO turns.
+     *
+     * @param actions the actions to display
+     * @param map the map to display
+     * @param display the object that performs the console I/O
+     * @return the Action that the user selects
+     *
+     */
     @Override
     public Action playTurn(Actions actions, GameMap map, Display display) {
         this.map = map;
@@ -27,14 +58,26 @@ public class GamePlayer extends Player {
 
     }
 
+    /**
+     * To change the status of stunnedPlayer
+     * @param stunStatus the new status to update to
+     */
     public void setPlayerStunned(boolean stunStatus) {
         stunnedPlayer = stunStatus;
     }
 
+    /**
+     * Returns the status of stunnedPlayer
+     * @return the current status of stunnedPlayer
+     */
     public boolean getPlayerStunned() {
         return stunnedPlayer;
     }
 
+    /**
+     * To remove player from the GameMap, This indicates the end of the game
+     * @param player the GamePlayer to be removed from the map
+     */
     public void removePlayerFromMap(GamePlayer player){
         map.removeActor(player);
     }
