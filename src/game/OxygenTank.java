6206c10220b0429430675232f1041bdd375045d5
megan o <mooi0004@student.monash.edu>
2019-05-14 14:49:12 +0800

    Added classes; OxygenTank, WaterPistol

--- src/game/OxygenTank.java
+++ src/game/OxygenTank.java
@@ -1,14 +1,13 @@
 package game;
 
-import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Item;
 
 public class OxygenTank extends Item {
 
     private MaxCounter oxygenPoint = new MaxCounter(11);
 
-    OxygenTank(Actor actor) {
-        Item oxygenTank = new Item("oxygen tank", 'o');
+    OxygenTank() {
+        super("oxygen tank", 'o');
     }
 
 }
---
+++ src/game/WaterPistol.java
@@ -0,0 +1,14 @@
+package game;
+
+import edu.monash.fit2099.engine.Item;
+
+public class WaterPistol extends Item{
+
+    private boolean isEmpty;
+
+    WaterPistol() {
+        super("water pistol", '¬');
+    }
+}
+
+
