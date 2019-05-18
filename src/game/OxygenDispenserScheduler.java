9f2e6e5d8b365e6c84168017ad92c8cc3df0a1ed
megan o <mooi0004@student.monash.edu>
2019-05-19 00:15:10 +0800

    OxygenDispenserScheduler- got rid of magic numbers

--- src/game/OxygenDispenserScheduler.java
+++ src/game/OxygenDispenserScheduler.java
@@ -6,6 +6,11 @@ public class OxygenDispenserScheduler extends Actor {
 
     Actor player;
 
+    // variables for the current turn number
+    private final int ROUND_ZERO = 0;
+    private final int ROUND_ONE = 1;
+    private final int ROUND_TWO = 2;
+
     private MaxCounter counter = new MaxCounter(3);
 
     OxygenDispenserScheduler(Actor player) {
@@ -15,11 +20,11 @@ public class OxygenDispenserScheduler extends Actor {
 
     @Override
     public Action playTurn(Actions actions, GameMap map, Display display) {
-        if (counter.getValue() == 1) {
+        if (counter.getValue() == ROUND_ONE) {
             counter.increment();
             return new SkipTurnAction();
         }
-        else if (counter.getValue() == 2) {
+        else if (counter.getValue() == ROUND_TWO) {
             return new DispenseOxygenTankAction(this);
         }
         return new SkipTurnAction();
@@ -28,7 +33,7 @@ public class OxygenDispenserScheduler extends Actor {
     @Override
     public Actions getAllowableActions(Actor otherActor, String direction, GameMap map) {
         Actions actions = new Actions();
-        if (counter.getValue() == 0) {
+        if (counter.getValue() == ROUND_ZERO) {
             actions.add(new PressButtonAction(otherActor, counter));
         }
         return actions;
