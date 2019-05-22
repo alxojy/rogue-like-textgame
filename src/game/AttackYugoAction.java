652f5ae3e7a8e4475dd7800465c1de8c28a48ffc
yhew0003 <yhew0003@student.monash.edu>
2019-05-22 22:47:16 +0800

    implemented the feature to check if player wins/ loses , prints end game message

--- src/game/AttackYugoAction.java
+++ src/game/AttackYugoAction.java
@@ -6,6 +6,7 @@ public class AttackYugoAction extends AttackAction {
 
     private YugoMaxx yugoMaxx;
     private GamePlayer player;
+    private Item waterPistol;
 
     public AttackYugoAction(GamePlayer player, YugoMaxx yugoMaxx) {
         super(player, yugoMaxx);
@@ -16,9 +17,21 @@ public class AttackYugoAction extends AttackAction {
     @Override
     public String execute(Actor actor, GameMap map) {
 
+        Weapon weapon = actor.getWeapon();
+        int damage = weapon.damage();
+
         if (yugoMaxx.hasExoskeleton()) {
             return actor + " attacks " + yugoMaxx + " but there's 0 damage ";
         }
+        yugoMaxx.hurt(damage);
+        if (!yugoMaxx.isConscious()) {
+            Item sleepingActor = new Item("Sleeping " + yugoMaxx, '%');
+            sleepingActor.addSkill(GameSkills.YUGOBODY);
+            map.locationOf(yugoMaxx).addItem(sleepingActor);
+            map.removeActor(yugoMaxx);
+            return yugoMaxx+" is knocked out";
+        }
+        yugoMaxx.heal(damage);
         return super.execute(actor, map);
     }
 }
