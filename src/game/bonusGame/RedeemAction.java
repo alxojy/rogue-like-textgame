af08d812ab1804f98fc337afaa4feeb1bfaec4b6
yhew0003 <yhew0003@student.monash.edu>
2019-05-30 18:24:52 +0800

    Added changes

--- src/game/bonusGame/RedeemAction.java
+++ src/game/bonusGame/RedeemAction.java
@@ -2,5 +2,14 @@ package game.bonusGame;
 
 import edu.monash.fit2099.engine.Action;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class RedeemAction extends Action {
+
+    private Map<BonusGameSkills, Integer> itemToStoneMap = new HashMap<BonusGameSkills, Integer>(); {
+    }
+    public boolean checkStones(){
+
+    }
 }
